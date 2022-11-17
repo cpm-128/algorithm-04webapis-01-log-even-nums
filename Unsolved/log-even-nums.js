45fbf9b1cf9bc17c5bfd77837e7f395b1f0c71d9
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2022-11-17 11:06:58 -0500

    console.log all nums <= num

--- Unsolved/log-even-nums.js
+++ Unsolved/log-even-nums.js
@@ -1,4 +1,10 @@
-// Write code to print all even numbers from 0 to `num`
-// Assume `num` will be a positive number
+//TODO: Write code to print all **even** numbers from 0 to `num`
+//! Assume `num` will be a positive number
 
-var logEvenNums = function(num) {};
+var logEvenNums = function(num) {
+    console.log(num);
+
+    for (i = 0; i <= num; i++) {
+        console.log(i);
+    }
+};
