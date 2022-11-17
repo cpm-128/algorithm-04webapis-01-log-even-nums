ed3bfd974a663d2f058c989d2410943b2227a696
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2022-11-17 11:25:57 -0500

    passes tests

--- Unsolved/log-even-nums.js
+++ Unsolved/log-even-nums.js
@@ -2,25 +2,19 @@
 //! Assume `num` will be a positive number
 
 var logEvenNums = function(num) {
-    console.log(num);
-
-    // // logs all nums 0 - num
-    // for (i = 0; i <= num; i++) {
-    //     console.log(i);
-    // };
+   // console.log(num);
 
     // loop through 0 - num
     for (i = 0; i <= num; i++) {
 
-        // find the remainder of num
+        // find the remainder of i
         var remainder = i % 2;
 
         // if the remainder !== 0, it is odd (bc we want 0/2=even)
         // else, it is even
         if (remainder !== 0) {
-            console.log(i);
         } else {
-            console.log('even');
+            console.log(i);
         }
     };
 
