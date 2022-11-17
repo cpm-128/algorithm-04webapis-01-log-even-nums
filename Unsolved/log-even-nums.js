94ee5c224ac22cf01ade0519c913f3969d662e8f
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2022-11-17 11:13:17 -0500

    psuedo code for odd/even

--- Unsolved/log-even-nums.js
+++ Unsolved/log-even-nums.js
@@ -4,7 +4,15 @@
 var logEvenNums = function(num) {
     console.log(num);
 
+    // logs all nums 0 - num
     for (i = 0; i <= num; i++) {
         console.log(i);
-    }
+    };
+
+    // find the remainder of num
+    var reaminder = num % 2;
+
+    // loop through 0 - num
+        // if the remainder !== 0, it is odd (bc we want 0/2=even)
+        // else, it is even
 };
